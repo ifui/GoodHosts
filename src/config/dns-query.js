3d510a1e8b8e9314628c1fd1bae107f614c09524
ifui <ifui@foxmail.com>
2021-08-20 19:21:23 +0800

    feat: 初步实现生成hosts文件

---
+++ src/config/dns-query.js
@@ -0,0 +1,9 @@
+/**
+ * 阿里云 https://dns.alidns.com/dns-query
+ * DNSPod https://doh.pub/dns-query
+ * 360 https://doh.360.cn/dns-query
+ */
+
+const dnsQuery = 'https://dns.alidns.com/dns-query'
+
+module.exports = dnsQuery
--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('hello')
+const doh = require('dohjs')
+const dnsQueryUrl = require('./config/dns-query')
+const githubUrls = require('./config/github')
+const fs = require('fs')
+
+const resolver = new doh.DohResolver(dnsQueryUrl)
+
+const getHosts = () => {
+  const promiseArr = githubUrls.map(async url => {
+    const {
+      answers
+    } = await resolver.query(url, 'A')
+    if (answers.length > 0) {
+      return answers[answers.length - 1].data + ' ' + url
+    }
+  })
+
+  Promise.all(promiseArr).then(res => {
+    const hosts = res.join('\r\n')
+    fs.writeFile('./hosts', hosts, (err) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+
+      console.log('hosts 文件更新成功')
+    })
+  })
+}
+
+getHosts()
